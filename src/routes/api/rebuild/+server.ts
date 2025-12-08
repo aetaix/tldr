49af6a0da9eff82530dce3d1ca686a5c85faf094
aetaix <adrien.etaix@hotmail.com>
2025-12-08 09:49:51 +0100

    refactor: comment out authorization check in GET function

--- src/routes/api/rebuild/+server.ts
+++ src/routes/api/rebuild/+server.ts
@@ -1,9 +1,9 @@
-import { CRON_SECRET } from "$env/static/private";
+// import { CRON_SECRET } from "$env/static/private";
 
-export function GET(req: Request) {
-	if (req.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
-		return new Response('Unauthorized', { status: 401 });
-	}
+export function GET() {
+	// if (req.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+	// 	return new Response('Unauthorized', { status: 401 });
+	// }
 	fetch(
 		'https://api.vercel.com/v1/integrations/deploy/prj_8yyCQKSHtjl9sdP0TjwAtPfIo3UK/IJwZ43iAVg',
 		{
